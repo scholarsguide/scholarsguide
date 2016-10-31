27f6843db0a7bad11e4c4f21c19a4c02c3523073
scholarsguide <scholarsguide.giis@gmail.com>
2016-10-31 10:32:45 +0800

    Delete firebaseAPI.js

--- firebaseAPI.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    apiKey: "AIzaSyDG2P8asZGhZLBQUy29zr5Lzn51JC2amZE",
-    authDomain: "scholars-guide.firebaseapp.com",
-    databaseURL: "https://scholars-guide.firebaseio.com",
-    storageBucket: "scholars-guide.appspot.com",
-    messagingSenderId: "876660691735"
-};
-firebase.initializeApp(config);
